b0ea74cfc40ee40d41ae801877912ade1a94fc2b
daniela <moreno.tobar.daniela@gmail.com>
2023-01-31 17:34:39 -0500

    put and delete

--- src/main/java/com/project/customers/student/StudentController.java
+++ src/main/java/com/project/customers/student/StudentController.java
@@ -16,14 +16,32 @@ public class StudentController {
         this.studentService = studentService;
     }
 
+    //1. Method All - Get
     @GetMapping
     public List<Student> getStudents(){
         return studentService.getStudents();
     }
 
+    //2. Method Add - Post
     @PostMapping
     public void registerNewStudent(@RequestBody Student student){
         studentService.addNewStudent(student);
+    }
+
+    //3. Method delete
+    @DeleteMapping(path = "{studentId}")
+    public void deleteStudent(@PathVariable("studentId") Long studentId){
+        studentService.deleteStudent(studentId);
+    }
+
+    //4. Method Edit - Put
+    @PutMapping(path = "{studentId}")
+    public void updateStudent(
+            @PathVariable("studentId") Long studentId,
+            @RequestParam(required = false) String name,
+            @RequestParam(required = false) String email
+    ){
+        studentService.updateStudent(studentId, name, email);
     }
 
 
--- src/main/java/com/project/customers/student/StudentService.java
+++ src/main/java/com/project/customers/student/StudentService.java
@@ -1,12 +1,14 @@
 package com.project.customers.student;
 
 
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
 import java.time.Month;
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 
 @Service
@@ -19,15 +21,45 @@ public class StudentService {
         this.studentRepository = studentRepository;
     }
 
+    //1. Method All - Get
     public List<Student> getStudents(){
         return studentRepository.findAll();
     }
 
+    //2. Method Add - Post
     public void addNewStudent(Student student) {
         Optional<Student> studentOptional = studentRepository.findStudentByEmail(student.getEmail());
         if (studentOptional.isPresent()){
             throw new IllegalStateException("email taken");
         }
         studentRepository.save(student);
+    }
+
+    //3. Method delete
+    public void deleteStudent(Long studentId) {
+        boolean exists = studentRepository.existsById(studentId);
+        if (!exists) {
+            throw new IllegalStateException("student with id " + studentId + " does not exists");
+        }
+        studentRepository.deleteById(studentId);
+    }
+
+    //Cuando usamos esta anotacion, la entity entra en un estado administrado
+    @Transactional
+    public void updateStudent(Long studentId, String name, String email) {
+        Student student = studentRepository.findById(studentId).orElseThrow(() -> new IllegalStateException(
+                "student whit id " + studentId + " does not exists"
+        ));
+        if (name != null && name.length() > 0 && !Objects.equals(student.getName(), name)){
+            student.setName(name);
+        }
+
+        if (email != null && email.length() > 0 && !Objects.equals(student.getEmail(), email)){
+            Optional<Student> studentOptional = studentRepository.findStudentByEmail(email);
+            if (studentOptional.isPresent()) {
+                throw new IllegalStateException("email taken");
+            }
+            student.setEmail(email);
+        }
     }
 }
